118d31efe5f79686b3035d345dce52643e6ad83d
abhishek patel <abhishekpatel6687.me>
2025-04-30 20:37:41 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,7 @@
 import React from "react";
 import {
-  FaFacebookF,
-  FaLinkedinIn,
-  FaInstagram,
-  FaTelegram,
-  FaYoutube,
-  FaGlobeAfrica,
-  FaTwitter,
+  FaFacebookF, FaLinkedinIn, FaInstagram, FaTelegram,
+  FaYoutube, FaGlobeAfrica, FaTwitter
 } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
 import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
@@ -17,61 +12,39 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
+      {/* Social Media & Logo Section */}
       <div className="section_1">
         <div className="logo">
           <img src="/image/logo-removebg-preview.png" alt="logo_footer" />
         </div>
         <div className="courses">
           <ul>
-            <li>
-              <FaLinkedinIn />
-            </li>
-            <li>
-              <FaInstagram />
-            </li>
-            <li>
-              <FaTwitter />
-            </li>
-            <li>
-              <FaFacebookF />
-            </li>
-            <li>
-              <FaTelegram />
-            </li>
-            <li>
-              <FaYoutube />
-            </li>
+            <li><FaLinkedinIn /></li>
+            <li><FaInstagram /></li>
+            <li><FaTwitter /></li>
+            <li><FaFacebookF /></li>
+            <li><FaTelegram /></li>
+            <li><FaYoutube /></li>
           </ul>
         </div>
       </div>
+
+      {/* Contact Info Section */}
       <div className="section_2">
         <div className="icon-list">
           <ul>
-            <li>
-              <HiOutlineBuildingOffice2 /> Babu Banarasi Das University
-            </li>
-            <li>
-              <IoHome /> BBD City, Faizabad Road, Lucknow Uttar Pradesh - 226
-              028 India
-            </li>
-            <li>
-              <MdCall /> 0-(522)-6196300/301/302
-            </li>
-            <li>
-              <MdCall /> 0-(522)-6196315/16/17/18
-            </li>
-            <li>
-              <TfiPrinter /> 0-(522)-6196222
-            </li>
-            <li>
-              <MdOutlineMailOutline /> info@bbdu.org
-            </li>
-            <li>
-              <FaGlobeAfrica /> https://bbdu.ac.in
-            </li>
+            <li><HiOutlineBuildingOffice2 /> Babu Banarasi Das University</li>
+            <li><IoHome /> BBD City, Faizabad Road, Lucknow Uttar Pradesh - 226028 India</li>
+            <li><MdCall /> 0-(522)-6196300/301/302</li>
+            <li><MdCall /> 0-(522)-6196315/16/17/18</li>
+            <li><TfiPrinter /> 0-(522)-6196222</li>
+            <li><MdOutlineMailOutline /> info@bbdu.org</li>
+            <li><FaGlobeAfrica /> https://bbdu.ac.in</li>
           </ul>
         </div>
       </div>
+
+      {/* Copyright */}
       <div className="last">
         <p>© Copyright 2018. All Rights Reserved by BBD University.</p>
       </div>
